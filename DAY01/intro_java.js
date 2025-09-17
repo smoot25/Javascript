d22b4226c813d582f8007e3549024f3615237b98
smoot25 <smoot1086@gmail.com>
2025-09-17 16:34:36 +0900

    자바스크립트 1일차

---
+++ DAY01/intro_java.js
@@ -0,0 +1,78 @@
+        /* 
+            객체 뒤에 붙는 명령문, 메소드
+            기본적으로 한줄에는 명령문 하나만 올 수 있음.
+            명령문들은 순차적으로 진행이 된다.
+            명령문 하나를 완벽하게 끝내기 위해 ;사용해야함.
+            입력 >> 처리 >> 출력
+        
+        window.alert("로그인을 먼저 진행해주세요!!")     // 경고창 제작 메소드
+        window.alert("해당 계정은 없는 계정입니다.")
+        */
+        
+        /*
+        // 정수 기록
+        console.log(10)
+        console.log(0)
+        console.log(-10)
+        // 실수 기록
+        console.log(1.8)
+        console.log(-0.8)
+        // 산술연산
+        console.log(10+2)
+        console.log(10%3)    // 나머지 연산자
+        // 자료형 확인하기
+        console.log(typeof 3)
+        */
+        
+        /*
+        let num = 5
+        console.log(num)
+        console.log(num)        
+
+        let number;
+        number =10;
+        console.log(number)
+
+        number = 18;
+        console.log(number)
+
+        const age = 20    // 이대로 완전히 고정
+        alert(age)
+        */
+
+        /*
+        let apple;
+        console.log(apple)     // 따옴표 사용하지 않을 경우, undefind
+        console.log('사과')
+        apple = '사과'
+        let banana;
+        console.log('바나나')
+        banana = '바나나'
+        console.log(apple + '&' + banana)
+
+        window.prompt('당신이 좋아하는 음료는?')    // 사용자 입력 창 생성
+        let answer = prompt('당신이 좋아하는 음료는?')
+        alert(answer)
+        */
+
+        /*
+        const modifier = '시원한'
+        const drink = `${modifier} ${prompt('좋아하는 음료는?')}`
+        console.log(drink)
+        */
+    
+        /*
+        let name;
+        name = '황준호'
+        console.log(name)
+
+        let answer = confirm('아이스아메리카노를 좋아하십니까?')
+        console.log(`아이스아메리카노를 좋아하십니까? ${answer}`)
+        */
+
+        /*
+        */
+
+        const p =document.querySelector('p')
+        console.log(p.textContent)
+        // p.textContent = ''를 실행하여 텍스트를 변경 가능하다.
